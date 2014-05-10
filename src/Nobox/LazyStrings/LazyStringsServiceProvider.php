a95da360a92a1cf862273e6008794421ec1f0068
Jonathan Torres <jonathantorres41@gmail.com>
2014-05-10 18:47:24 -0400

    Some indentation changes.
    Laravel generates files in tabs.

--- src/Nobox/LazyStrings/LazyStringsServiceProvider.php
+++ src/Nobox/LazyStrings/LazyStringsServiceProvider.php
@@ -4,50 +4,50 @@ use Illuminate\Support\ServiceProvider;
 
 class LazyStringsServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('nobox/lazy-strings');
-	}
+    /**
+     * Bootstrap the application events
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('nobox/lazy-strings');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		// add LazyStrings class to app container
-		$this->app['lazy-strings'] = $this->app->share(function($app) {
-			return new LazyStrings();
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // add LazyStrings class to app container
+        $this->app['lazy-strings'] = $this->app->share(function($app) {
+            return new LazyStrings();
+        });
 
-		// add class alias
-		$this->app->booting(function() {
-			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        // add class alias
+        $this->app->booting(function() {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('LazyStrings', 'Nobox\LazyStrings\LazyStrings');
-		});
-	}
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('lazy-strings');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('lazy-strings');
+    }
 
 }
