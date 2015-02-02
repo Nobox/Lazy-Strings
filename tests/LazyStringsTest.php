d8c61785aa25c6053a21b030a3e8334f438b2670
Jonathan Torres <jonathantorres41@gmail.com>
2015-02-02 14:16:56 -0500

    Update tests.

--- tests/LazyStringsTest.php
+++ tests/LazyStringsTest.php
@@ -28,6 +28,8 @@ class LazyStringsTest extends Orchestra\Testbench\TestCase
         $this->file->shouldReceive('put')->atLeast()->times(1);
 
         $this->lazyStrings->setCsvUrl('http://docs.google.com/spreadsheets/d/1V_cHt5Fe4x9XwVepvlXB39sqKXD3xs_QbM-NppkrE4A/export?format=csv');
+        $this->lazyStrings->setSheets(array('en' => 0));
+
         $generated = $this->lazyStrings->generate();
 
         $this->assertArrayHasKey('en', $generated, 'Strings array not parsed correctly.');
