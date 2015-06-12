a6c89b5c406acce3b3b85611b22ea3ab2852231c
Jonathan Torres <jonathantorres41@gmail.com>
2015-06-12 16:16:43 -0400

    Add string helper.

---
+++ tests/Helpers/StrTest.php
@@ -0,0 +1,31 @@
+<?php namespace Nobox\LazyStrings\Tests\Helpers;
+
+use Nobox\LazyStrings\Helpers\Str;
+use Orchestra\Testbench\TestCase;
+
+class StrTest extends TestCase
+{
+    protected $str;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->str = new Str();
+    }
+
+    public function testStripNewlines()
+    {
+        $newline = $this->str->strip("some.key.here\n");
+        $newlines = $this->str->strip("\nsome.key.here\n\n");
+        $normal = $this->str->strip("some.key.here");
+        $return = $this->str->strip("some.key.here\r");
+        $returns = $this->str->strip("\r\rsome.key.here\r");
+
+        $this->assertSame('some.key.here', $newline);
+        $this->assertSame('some.key.here', $newlines);
+        $this->assertSame('some.key.here', $normal);
+        $this->assertSame('some.key.here', $return);
+        $this->assertSame('some.key.here', $returns);
+    }
+}
