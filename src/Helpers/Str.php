c1e435555bd5b902764f7a6ee608bd58323252a6
Jonathan Torres <jonathan@nobox.com>
2015-11-12 18:59:58 -0500

    Refactor array conversion method.

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -38,35 +38,12 @@ class Str
         // Support up to 5 dimensions? (one.two.three.four.five)
 
         if ($dimensions > 0) {
-            // add first dimension
-            $result[$keys[0]] = [];
-
-            for ($i = 1; $i < $dimensions; $i++) {
-                $emptyItem = [$keys[$i] => []];
-
-                if ($i === 1) { // two dimensions
-                    $result[$keys[$i - 1]] = $emptyItem;
-                } elseif ($i === 2) { // three dimensions
-                    $result[$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
-                } elseif ($i === 3) { // four dimensions
-                    $result[$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
-                } elseif ($i === 4) { // five dimensions
-                    $result[$keys[$i - 4]][$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
-                }
-            }
-
-            // append value to last item, depending on dimension
-            if ($dimensions === 1) {
-                $result[$keys[$dimensions - 1]] = $value;
-            } elseif ($dimensions === 2) {
-                $result[$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-            } elseif ($dimensions === 3) {
-                $result[$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-            } elseif ($dimensions === 4) {
-                $result[$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-            } elseif ($dimensions === 5) {
-                $result[$keys[$dimensions - 5]][$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-            }
+            $result = $this->appendStringValueToLastItem(
+                $this->buildDimensionalArray($keys, $dimensions),
+                $keys,
+                $dimensions,
+                $value
+            );
         }
 
         return $result;
@@ -86,5 +63,65 @@ class Str
         }
 
         return true;
+    }
+
+    /**
+     * Builds nested array with the specified keys.
+     *
+     * @param array $keys
+     * @param int $dimensions
+     *
+     * @return array
+     */
+    private function buildDimensionalArray(array $keys, $dimensions)
+    {
+        $result = [];
+
+        // add first dimension
+        $result[$keys[0]] = [];
+
+        for ($i = 1; $i < $dimensions; $i++) {
+            $emptyItem = [$keys[$i] => []];
+
+            if ($i === 1) { // two dimensions
+                $result[$keys[$i - 1]] = $emptyItem;
+            } elseif ($i === 2) { // three dimensions
+                $result[$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
+            } elseif ($i === 3) { // four dimensions
+                $result[$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
+            } elseif ($i === 4) { // five dimensions
+                $result[$keys[$i - 4]][$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Append key value to the last nested item on the array.
+     * Depending on the dimensions.
+     *
+     * @param array $results
+     * @param array $keys
+     * @param int $dimensions
+     * @param string $value
+     *
+     * @return array
+     */
+    private function appendStringValueToLastItem($results, $keys, $dimensions, $value)
+    {
+        if ($dimensions === 1) {
+            $result[$keys[$dimensions - 1]] = $value;
+        } elseif ($dimensions === 2) {
+            $result[$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
+        } elseif ($dimensions === 3) {
+            $result[$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
+        } elseif ($dimensions === 4) {
+            $result[$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
+        } elseif ($dimensions === 5) {
+            $result[$keys[$dimensions - 5]][$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
+        }
+
+        return $result;
     }
 }
