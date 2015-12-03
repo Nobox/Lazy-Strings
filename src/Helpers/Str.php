d2003c4965be51db388e161a3db8d140bd75bd48
Jonathan Torres <jonathan@nobox.com>
2015-12-03 16:52:00 -0500

    Improve construction of nested dimensional arrays.

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -6,7 +6,6 @@ use Exception;
 
 class Str
 {
-
     /**
      * Strip newlines and returns from string.
      *
@@ -22,7 +21,6 @@ class Str
     /**
      * Convert dotted string to a nested array.
      * Append the value to the last nested array.
-     * Currently supporting up to 5 dimensions.
      *
      * @param string $string
      * @param string $value
@@ -31,27 +29,9 @@ class Str
      */
     public function convertToArray($string, $value)
     {
-        $result = [];
         $keys = explode('.', $string);
-        $dimensions = count($keys);
-        $supportedDimensions = 5;
 
-        // check if dimensions is larger than the amount of allowed dimensions
-        if ($dimensions > $supportedDimensions) {
-            $message = 'Too many dimensions. Currently supporting up to ' . $supportedDimensions . ' dimensions.';
-            throw new Exception($message);
-        }
-
-        if ($dimensions > 0) {
-            $result = $this->appendValueToLastItem(
-                $this->buildDimensionalArray($keys, $dimensions),
-                $keys,
-                $dimensions,
-                $value
-            );
-        }
-
-        return $result;
+        return $this->buildDimensionalArray($keys, count($keys), $value);
     }
 
     /**
@@ -75,56 +55,25 @@ class Str
      *
      * @param array $keys
      * @param int $dimensions
+     * @param string $value
      *
      * @return array
      */
-    private function buildDimensionalArray(array $keys, $dimensions)
+    private function buildDimensionalArray(array $keys, $dimensions, $value)
     {
         $result = [];
 
         // add first dimension
         $result[$keys[0]] = [];
+        $pointer = &$result[$keys[0]];
 
         for ($i = 1; $i < $dimensions; $i++) {
-            $emptyItem = [$keys[$i] => []];
-
-            if ($i === 1) { // two dimensions
-                $result[$keys[$i - 1]] = $emptyItem;
-            } elseif ($i === 2) { // three dimensions
-                $result[$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
-            } elseif ($i === 3) { // four dimensions
-                $result[$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
-            } elseif ($i === 4) { // five dimensions
-                $result[$keys[$i - 4]][$keys[$i - 3]][$keys[$i - 2]][$keys[$i - 1]] = $emptyItem;
+            if ($i === ($dimensions - 1)) {
+                $pointer[$keys[$i]] = $value;
+            } else {
+                $pointer[$keys[$i]] = [];
+                $pointer = &$pointer[$keys[$i]];
             }
-        }
-
-        return $result;
-    }
-
-    /**
-     * Append key value to the last nested item on the array.
-     * Depending on the dimensions.
-     *
-     * @param array $results
-     * @param array $keys
-     * @param int $dimensions
-     * @param string $value
-     *
-     * @return array
-     */
-    private function appendValueToLastItem($results, $keys, $dimensions, $value)
-    {
-        if ($dimensions === 1) {
-            $result[$keys[$dimensions - 1]] = $value;
-        } elseif ($dimensions === 2) {
-            $result[$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-        } elseif ($dimensions === 3) {
-            $result[$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-        } elseif ($dimensions === 4) {
-            $result[$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
-        } elseif ($dimensions === 5) {
-            $result[$keys[$dimensions - 5]][$keys[$dimensions - 4]][$keys[$dimensions - 3]][$keys[$dimensions - 2]][$keys[$dimensions - 1]] = $value;
         }
 
         return $result;
